4c3a6beba5bc968bea4b918861a87be866824332
ThomasDale1 <dalethomas1212@gmail.com>
2026-02-02 20:23:37 -0600

    router setup for products

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import productsRouter from './routes/products/index.ts'
 
 const port = 3000
 const app = express()
@@ -6,6 +7,8 @@ const app = express()
 app.get('/', (req, res) => {
     res.send('Hello Express')
 })
+
+app.use('/products', productsRouter)
 
 app.listen(port, () => {
     console.log('Server listening on port ' + port)
