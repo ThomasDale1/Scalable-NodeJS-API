784828cac0e433aa6e33086ed8adcde1a36403e5
ThomasDale1 <dalethomas1212@gmail.com>
2026-02-04 19:39:01 -0600

    orders CRUD

--- api/src/routes/orders/ordersController.ts
+++ api/src/routes/orders/ordersController.ts
@@ -2,6 +2,7 @@ import { json } from "drizzle-orm/gel-core";
 import { Request, Response } from "express";
 import { orderItemsTable, ordersTable } from "../../db/ordersSchema.js";
 import { db } from "../../db/index.js";
+import { eq } from "drizzle-orm/index.js";
 
 export async function createOrder(req: Request, res: Response){
     try{
@@ -10,6 +11,7 @@ export async function createOrder(req: Request, res: Response){
         if(!userId){
             res.status(400).json({message: 'Invalid order data'})
         }
+        //@ts-ignore
         const [newOrder] = await db.insert(ordersTable).values({userId: userId}).returning()
 
         const orderItems = items.map((item: any) => ({
@@ -20,5 +22,46 @@ export async function createOrder(req: Request, res: Response){
         res.status(201).json({...newOrder, items: newOrderItems})
     }catch(e){
         res.status(400).json({message: 'Invalid order data'})
+    }
+}
+
+export async function listOrders(req: Request, res: Response){
+    try{
+        const orders = await db.select().from(ordersTable)
+        res.json(orders)
+    }catch(error){
+        res.status(500).send(error)
+    }
+}
+
+export async function getOrder(req: Request, res: Response){
+    try{
+        //@ts-ignore
+        const id = parseInt(req.params.id)
+        const orderWithItems = await db.select().from(ordersTable).where(eq(ordersTable.id, id)).leftJoin(orderItemsTable, eq(ordersTable.id, orderItemsTable.orderId))
+        if(orderWithItems.length == 0){
+            res.status(404).send('order not found')
+        }
+        const mergedOrder = {
+            ...orderWithItems[0].orders, items:orderWithItems.map((oi) => oi.order_items)
+        }
+        res.status(200).json(mergedOrder)
+    }catch(error){
+        res.status(500).send(error)
+    }
+}
+
+export async function updateOrder(req: Request, res: Response){
+    try{
+        //@ts-ignore
+        const id = parseInt(req.params.id)
+        const [updatedOrder] = await db.update(ordersTable).set(req.body).where(eq(ordersTable.id, id)).returning()
+        if(!updatedOrder){
+            res.status(404).send('Order not found')
+        }else{
+            res.status(200).json(updatedOrder)
+        }
+    }catch(error){
+        res.status(500).send(error)
     }
 }
