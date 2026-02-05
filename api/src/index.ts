7d31a5f05ce229bcd023cea398e04d120b70c283
ThomasDale1 <dalethomas1212@gmail.com>
2026-02-04 18:37:15 -0600

    order items created

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,6 +1,7 @@
 import express, {json, urlencoded} from 'express'
 import productsRouter from './routes/products/index.js'
 import authRoutes from './routes/auth/index.js'
+import ordersRoutes from './routes/orders/index.js'
 
 const port = 3000
 const app = express()
@@ -13,6 +14,7 @@ app.use(urlencoded({extended: false}))
 app.use(json())
 app.use('/products', productsRouter)
 app.use('/auth', authRoutes)
+app.use('/orders', ordersRoutes)
 
 app.listen(port, () => {
     console.log('Server listening on port ' + port)
